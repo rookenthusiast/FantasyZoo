fe2e4ce554397d5cb78878f8a380d8a2a6f1e540
Cameron Fulton <rookenthusiast@gmail.com>
2016-12-19 12:02:55 +0000

    welcome page created,zoo hub page started

--- app/src/main/java/com/example/user/fantasyzoo/WelcomeActivity.java
+++ app/src/main/java/com/example/user/fantasyzoo/WelcomeActivity.java
@@ -1,8 +1,10 @@
 package com.example.user.fantasyzoo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -11,20 +13,24 @@ import android.widget.TextView;
 public class WelcomeActivity extends AppCompatActivity {
     TextView welcomeText;
     Button welcomeButton;
-}
 
-@Override
-protected void onCreate(Bundle saveInstancesState){
-    super.onCreate(saveInstancesState);
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+    super.onCreate(saveInstanceState);
     setContentView(R.layout.activity_welcome);
 
-    welcomeButton = (Button) findViewById(R.id.enter_button);
+    welcomeText = (TextView)findViewById(R.id.welcome_text);
+    welcomeButton = (Button)findViewById(R.id.enter_button);
 
-    Button.setOnClickListener(new View.OnClickListener(){
+    welcomeButton.setOnClickListener(new View.OnClickListener(){
+
         @Override
                 public void onClick(View v) {
-            Intent intent = new WelcomeActivity.this, FantasyZoo.class)
-            startActivity();
-        });
-    }
+            Intent intent = new Intent(WelcomeActivity.this, FantasyZoo.class);
+            startActivity(intent);
+        }
+    });
+  }
 }
+
+
